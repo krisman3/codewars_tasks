8b2450aadaf515377e0d2be2129c955d28359c50
KIliev <kriskokz@gmail.com>
2025-02-28 10:44:46 +0200

    took care of the empty number case

--- narcissistic_number.py
+++ narcissistic_number.py
@@ -1,6 +1,11 @@
 def narcissistic(value):
-    num_length = len(str(value))
+    str_int = str(value)
+    num_length = len(str_int)
+    powered_full_num = 0
+    if num_length == 0:
+        return False
+    for i in str_int:
 
 
 if __name__ == "__main__":
-    print(narcissistic(123))
+    print(narcissistic(153))
