a06f2f5d89f3d031da61f505dedd891628b77e4f
KIliev <kriskokz@gmail.com>
2025-02-28 10:51:22 +0200

    task completed and checked on codewars.com

--- narcissistic_number.py
+++ narcissistic_number.py
@@ -2,10 +2,13 @@ def narcissistic(value):
     str_int = str(value)
     num_length = len(str_int)
     powered_full_num = 0
+    summed = []
     if num_length == 0:
         return False
     for i in str_int:
+        summed.append(int(i) ** num_length)
 
+    return value == sum(summed)
 
 if __name__ == "__main__":
-    print(narcissistic(153))
+    print(narcissistic(1938))
