31eaf3b66e492f0d362b5592a2dd7987fc739c8f
KIliev <kriskokz@gmail.com>
2025-03-04 14:51:22 +0200

    fully working with different types

--- unique_in_order.py
+++ unique_in_order.py
@@ -1,10 +1,12 @@
 def unique_in_order(sequence):
-    new_string = ''
-    for i in sequence:
-        if i not in new_string:
-            new_string += i
-        else:
-            continue
+    if not sequence:
+        return []
+
+    new_string = [sequence[0]]
+    for i in range(1, len(sequence)):
+        if sequence[i] != sequence[i - 1]:
+            new_string.append(sequence[i])
+
     return new_string
 
 
