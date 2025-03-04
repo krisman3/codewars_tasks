1d5d79e40568fa56533595a9f41241054ff7fe3c
KIliev <kriskokz@gmail.com>
2025-03-04 14:18:44 +0200

    just returns unique characters - not the proper solution

--- unique_in_order.py
+++ unique_in_order.py
@@ -1,6 +1,11 @@
 def unique_in_order(sequence):
     new_string = ''
-
+    for i in sequence:
+        if i not in new_string:
+            new_string += i
+        else:
+            continue
+    return new_string
 
 
 if __name__ == "__main__":
