a3b7687a26e3e7dd3d27d3f7289690ea73a07829
KIliev <kriskokz@gmail.com>
2025-03-04 11:35:45 +0200

    completed 3 unit tests

--- persistence.py
+++ persistence.py
@@ -32,6 +32,7 @@ class TestNumbers(unittest.TestCase):
     def test_numbers(self):
         self.assertEqual(persistence(39), 3)
         self.assertEqual(persistence(4), 0)
-        
+        self.assertEqual(persistence(999), 4)
+
 if __name__ == "__main__":
     unittest.main()
