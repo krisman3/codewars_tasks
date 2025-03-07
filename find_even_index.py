9f9f74214735d8528f83a8301ce94a966d334be4
KIliev <kriskokz@gmail.com>
2025-03-07 09:36:29 +0200

    attempt at solution - not working

--- find_even_index.py
+++ find_even_index.py
@@ -5,5 +5,20 @@ the integers to the left of N is equal to the sum of the integers to the right o
 If there is no index that would make this happen, return -1.
 """
 
+
 def find_even_index(arr):
-    pass
+    if not arr:
+        return -1
+    temp_sum_left = 0
+    temp_sum_right = 0
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1, i, -1):
+            temp_sum_left += int(arr[i])
+            temp_sum_right += int(arr[j])
+        if temp_sum_right == temp_sum_left:
+            return i + 1
+    return f"left: {temp_sum_left} / right: {temp_sum_right}"
+
+
+if __name__ == "__main__":
+    print(find_even_index([1, 2, 3, 4, 3, 2, 1]))
