bd7deab1523905787dcb88fba7c714b99dc01910
KIliev <kriskokz@gmail.com>
2025-03-12 17:11:55 +0200

    completed and works for 109/109 cases on codewars

--- sort_odd.py
+++ sort_odd.py
@@ -10,8 +10,9 @@ Examples
 """
 
 
+# Works only for [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 def sort_array(source_array):
-    new_array = []
+    """new_array = []
     final_array = []
     for i in source_array:
         if i % 2 != 0:
@@ -28,7 +29,30 @@ def sort_array(source_array):
             j += 1
         except IndexError:
             pass
-    print(final_array)
+    print(final_array)"""
+
+    # with a 2 step iteration
+    odd_array = []
+    final_array = []
+    for i in source_array:
+        if i % 2 != 0:
+            odd_array.append(i)
+        else:
+            pass
+    odd_array = sorted(odd_array)
+
+    for j in range(len(source_array)):
+        # check if j is even
+        if source_array[j] % 2 == 0:
+            final_array.append(source_array[j])
+            # grab the even number from source_array
+        else:
+            # grab the first item from odd_array
+            final_array.append(odd_array[0])
+            odd_array.pop(0)
+
+    return final_array
+
 
 if __name__ == "__main__":
-    sort_array([7,1])
+    sort_array([5, 8, 6, 3, 4])
