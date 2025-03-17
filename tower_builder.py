3f95ddc7d5b246741d742705e13f3c117cd0ba81
KIliev <kriskokz@gmail.com>
2025-03-17 09:56:55 +0200

    accounted for spaces and completed task

--- tower_builder.py
+++ tower_builder.py
@@ -14,18 +14,14 @@ For example, a tower with 3 floors looks like this:
 
 def tower_builder(n_floors):
     tower = [] * n_floors
-    # for k in range(n_floors):
-    #     tower[k] =
-    #     tower.append('*' * (k * 2 + 1))
-
-    # return tower
 
     for i in range(n_floors):
-        if i == 0:
-            tower.append('*')
-        tower.append('*' * (i + 2))
+        asterisks = '*' * (i * 2 + 1)
+        spaces = ' ' * (n_floors - i - 1)
+        tower.append(spaces + asterisks + spaces)
+
     return tower
 
 
 if __name__ == '__main__':
-    print(tower_builder(6))
+    print(tower_builder(10))
