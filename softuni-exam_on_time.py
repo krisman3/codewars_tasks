4f96e248c3c67d2b420415aa365f8dadfa59632b
KIliev <kriskokz@gmail.com>
2025-04-30 14:02:34 +0300

    totally broke it, but reworking - need to think on how to handle total minutes

--- softuni-exam_on_time.py
+++ softuni-exam_on_time.py
@@ -4,41 +4,27 @@ exam_minute = int(input())
 arrival_hour = int(input())
 arrival_minute = int(input())
 
-hour_diff = 0
-minute_diff = 0
 
-# Positive cases
-# Exact time
-if exam_hour == arrival_hour and exam_minute == arrival_minute:
+print_hour = 0
+print_minute = 0
+
+final_exam_minutes = (exam_hour * 60) + exam_minute
+final_arrival_minutes = (arrival_hour * 60) + arrival_minute
+final_difference = final_exam_minutes - final_arrival_minutes
+if final_difference * -1 > 59:
+    print_hour = final_difference // 60
+    print_minute = final_difference % 60
+    ### stuck here
+
+print(f"final exam minutes: {final_exam_minutes}\n"
+      f"final arrival minutes: {final_arrival_minutes}\n"
+      f"final difference: {final_difference}")
+
+print(f"{final_exam_minutes} / {final_arrival_minutes}")
+
+if final_arrival_minutes == final_exam_minutes:
     print("On time")
-
-elif exam_hour >= arrival_hour:
-    if exam_minute >= arrival_minute:
-        hour_diff = exam_hour - arrival_hour
-        minute_diff = exam_minute - arrival_minute
-        if hour_diff == 0:
-            print(f"On time\n{minute_diff:02d} minutes before start")
-        else:
-            print(f"On time\n{hour_diff}:{minute_diff:02d} hours before the start")
-    if exam_minute < arrival_minute:
-        hour_diff = exam_hour - arrival_hour
-        if hour_diff == 0:
-            minute_diff = 60 - arrival_minute
-            print(f"On time\n{minute_diff:02d} minutes before start")
-        else:
-            print(f"On time\n{hour_diff}:{minute_diff:02d} hours before the start")
-
-# Negative cases with the same hour
-elif exam_hour == arrival_hour:
-    if exam_minute < arrival_minute:
-        minute_diff = arrival_minute - exam_minute
-        print(f"Late\n{minute_diff:02d} minutes after the start")
-
-
-# Negative cases with different hour
-elif exam_hour < arrival_hour:
-    minute_diff = (60 - exam_minute) + arrival_minute
-    if minute_diff >= 60:
-        minute_diff -= 60
-        hour_diff = arrival_hour - exam_hour
-        print(f"Late\n{hour_diff}:{minute_diff:02d} hours after the start")
+elif final_exam_minutes > final_arrival_minutes:
+    print(f"On time\n{final_difference // 60}:{final_difference%60:02d}")
+elif final_exam_minutes < final_arrival_minutes:
+    print(f"Late\n{final_difference // 60}:{final_difference%60:02d}")
