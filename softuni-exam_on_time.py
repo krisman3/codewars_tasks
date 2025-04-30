4689aa880249e0579b82f9563b6f2618f572ff54
KIliev <kriskokz@gmail.com>
2025-04-30 11:40:34 +0300

    positive cases broken when arrival_minute > exam_minute

--- softuni-exam_on_time.py
+++ softuni-exam_on_time.py
@@ -20,15 +20,23 @@ elif exam_hour >= arrival_hour:
             print(f"On time\n{minute_diff:02d} minutes before start")
         else:
             print(f"On time\n{hour_diff}:{minute_diff:02d} hours before the start")
+    if exam_minute < arrival_minute:
+        hour_diff = exam_hour - arrival_hour
+        if hour_diff == 0:
+            minute_diff = 60 - arrival_minute
+            print(f"On time\n{minute_diff:02d} minutes before start")
+        else:
+            print(f"On time\n{hour_diff}:{minute_diff:02d} hours before the start")
+
 # Negative cases with the same hour
-if exam_hour == arrival_hour:
+elif exam_hour == arrival_hour:
     if exam_minute < arrival_minute:
         minute_diff = arrival_minute - exam_minute
         print(f"Late\n{minute_diff:02d} minutes after the start")
 
 
 # Negative cases with different hour
-if exam_hour < arrival_hour:
+elif exam_hour < arrival_hour:
     minute_diff = (60 - exam_minute) + arrival_minute
     if minute_diff >= 60:
         minute_diff -= 60
