315b610a1af920fd42c0565d14e5f62a441e8517
KIliev <kriskokz@gmail.com>
2025-05-08 10:42:21 +0300

    starting over with the help of the lecturer

--- softuni-exam_on_time.py
+++ softuni-exam_on_time.py
@@ -1,30 +1,10 @@
-exam_hour = int(input())
-exam_minute = int(input())
+exam_start_hour = int(input()) # 10
+exam_start_minutes = int(input()) # 30
 
-arrival_hour = int(input())
-arrival_minute = int(input())
+arrival_hour = int(input()) # 10
+arrival_minutes = int(input()) # 00
 
 
-print_hour = 0
-print_minute = 0
+total_minutes_start_exam = (exam_start_hour * 60) + exam_start_minutes
+total_minutes_arrival = (arrival_hour * 60) + arrival_minutes
 
-final_exam_minutes = (exam_hour * 60) + exam_minute
-final_arrival_minutes = (arrival_hour * 60) + arrival_minute
-final_difference = final_exam_minutes - final_arrival_minutes
-if final_difference * -1 > 59:
-    print_hour = final_difference // 60
-    print_minute = final_difference % 60
-    ### stuck here
-
-print(f"final exam minutes: {final_exam_minutes}\n"
-      f"final arrival minutes: {final_arrival_minutes}\n"
-      f"final difference: {final_difference}")
-
-print(f"{final_exam_minutes} / {final_arrival_minutes}")
-
-if final_arrival_minutes == final_exam_minutes:
-    print("On time")
-elif final_exam_minutes > final_arrival_minutes:
-    print(f"On time\n{final_difference // 60}:{final_difference%60:02d}")
-elif final_exam_minutes < final_arrival_minutes:
-    print(f"Late\n{final_difference // 60}:{final_difference%60:02d}")
