09ae02e01c10b25e0facb20c945f8e6fa502f9bf
KIliev <kriskokz@gmail.com>
2025-05-08 15:52:39 +0300

    continue adding logic

--- softuni-exam_on_time.py
+++ softuni-exam_on_time.py
@@ -7,4 +7,18 @@ arrival_minutes = int(input()) # 00
 
 total_minutes_start_exam = (exam_start_hour * 60) + exam_start_minutes
 total_minutes_arrival = (arrival_hour * 60) + arrival_minutes
+difference = total_minutes_start_exam - total_minutes_arrival
 
+if difference == 0:
+    print("On time")
+
+elif 0 < difference <= 30:
+    print("On time")
+    print(f"{difference} minutes before the start")  # Time needs to be added
+
+elif 30 < difference <= 59:
+    print("Early")
+    print(f"{difference}")
+
+elif difference > 59:
+    pass  # Logic for early by more than hour to be added 
