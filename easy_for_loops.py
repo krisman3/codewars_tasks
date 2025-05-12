c5ad72bf89e82995fe4701ca41c6fdad75714912
KIliev <kriskokz@gmail.com>
2025-05-12 09:37:53 +0300

    logic with init numbers fixed

--- easy_for_loops.py
+++ easy_for_loops.py
@@ -3,17 +3,21 @@ n = int(input())
 
 left_list = []
 right_list = []
+
+
 for i in range(n):
     left_list.append(int(input()))
+for i in range(n):
     right_list.append(int(input()))
+
+print(left_list)
+print(right_list)
     
 left = sum(left_list)
 right = sum(right_list)
 
-print(f'left: {left} / right: {right}')
-
 if left == right:
-    print(f'Yes, sum = {left+right}')
+    print(f'Yes, sum = {left}')
 else:
     if left > right:
         print(f'No, diff = {left - right}')
