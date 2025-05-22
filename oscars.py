ce0a861c380aafdad4eac507e51ad70316b16486
KIliev <kriskokz@gmail.com>
2025-05-22 11:21:18 +0300

    debugging

--- oscars.py
+++ oscars.py
@@ -7,7 +7,10 @@ for _ in range(n):
     judge_name = input()
     for letter in judge_name:
         letters_in_judge_name += 1
-    judge_points = (float(input()) * letters_in_judge_name) + points
+    judge_points = float(input())
+    judge_points = points + ((judge_points * letters_in_judge_name) / 2) #(float(input()) * letters_in_judge_name) + points
+print(f'2. {letters_in_judge_name}')
+print(f'3. {judge_points}')
 
 
 if points > 1250.5:
