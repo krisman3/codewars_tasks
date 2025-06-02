646d0824e9eb46bea7e329586bbb71392e97e415
KIliev <kriskokz@gmail.com>
2025-06-02 17:18:23 +0300

    works with a single pass + bonus output

--- chatgpt_task2.py
+++ chatgpt_task2.py
@@ -9,8 +9,8 @@ Write a function has_duplicates(items: list) -> bool that checks whether the inp
 
 Bonus: Modify it to return a list of all duplicates (each only once).
 """
-
-
+# Working solution, but with higher complexity
+"""
 def has_duplicates(items: list) -> bool:
     duplicates = []
     for i in range(len(items)):
@@ -20,7 +20,24 @@ def has_duplicates(items: list) -> bool:
     if duplicates:
         return True
     return False
+"""
+
+# Attempt at a single pass + bonus output
+
+def has_duplicates(items: list) -> bool:
+    duplicates = {}
+    set_duplicates = set()
+    for i in items:
+        if i not in duplicates.keys():
+            duplicates[i] = 1
+        else:
+            duplicates[i] += 1
+            set_duplicates.add(i)
+    if set_duplicates:
+        print(set_duplicates)
+        return True
+    return False
 
         
 
-print(has_duplicates([1, 2, 3, 4]))
+print(has_duplicates([1, 2, 3, 4, 1, 3, 2]))
