9ff1d933df7b9d592b943dcb56264d81a7bd7a12
KIliev <kriskokz@gmail.com>
2025-06-04 12:16:59 +0300

    finished task 2 from while loops

--- while_loop_exercises.py
+++ while_loop_exercises.py
@@ -8,5 +8,19 @@ while continuous:
     else:
         continuous = False
 
-"""
 
+####
+
+name = input()
+password = input()
+
+security = True
+while security:
+    pass_input = input()
+    if pass_input == password:
+        print(f"Welcome {name}!")
+        security = False
+    else:
+        continue
+
+"""
