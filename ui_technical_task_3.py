20e850af834e6ae0d59709285e72005591855fa5
KIliev <kriskokz@gmail.com>
2025-06-10 09:47:11 +0300

    task 3 for the interview - works

--- ui_technical_task_3.py
+++ ui_technical_task_3.py
@@ -3,16 +3,21 @@ class BankAccount:
         self.balance = 0
 
     def deposit(self, amount):
-        pass
+        self.balance += amount
+        return f'{amount} lv. deposited!'
 
     def withdraw(self, amount):
-        pass
+        if amount > self.balance:
+            return f'Insufficient funds!'
+        else:
+            new_balance = self.balance - amount
+            return f'{amount} lv. withdrawn. New balance: {new_balance}'
 
     def check_balance(self):
         return f'{self.balance} lv.'
 
 
-
 new_bank_account = BankAccount()
+print(new_bank_account.deposit(15))
+print(new_bank_account.withdraw(14))
 print(new_bank_account.balance)
-
