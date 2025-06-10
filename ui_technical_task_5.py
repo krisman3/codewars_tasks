78e17353d03628d600b9be8f038c5de9c4abd709
KIliev <kriskokz@gmail.com>
2025-06-10 13:00:42 +0300

    working on task 5

---
+++ ui_technical_task_5.py
@@ -0,0 +1,26 @@
+def check_user_access(user: dict):
+    if user['is_active'] is False:
+        return 'Access denied: Inactive account'
+    elif user['age'] < 18:
+        return 'Access denied: Underage'
+    elif user['age'] >= 18 and user['is_active'] is True:
+        curr_role = user['roles']
+        if curr_role == 'admin':
+            return 'Access granted: Admin'
+        elif curr_role == 'editor':
+            return 'Access granted: Editor'
+    else:
+        return f'Access granted: {user['username']}'
+
+
+# happy path ADMIN
+user_dict = {'username': 'Max88verstappen', 'age': 18, 'is_active': True, 'roles': ['admin']}
+
+# inactive account
+inactive_user = {'username': 'joro_pochivka', 'age': 18, 'is_active': False, 'roles': ['admin']}
+
+# happy path EDITOR
+editor_dict = {'username': 'doom_guy', 'age': 18, 'is_active': True, 'roles': ['admin']}
+
+# underaged acc
+editor_dict = {'username': 'doom_guy', 'age': 18, 'is_active': True, 'roles': ['admin']}
