68237eba7f199d5ec73c0c1ecb875723c3b25684
KIliev <kriskokz@gmail.com>
2025-06-10 13:19:12 +0300

    task 5 fixed and tested

--- ui_technical_task_5.py
+++ ui_technical_task_5.py
@@ -4,13 +4,13 @@ def check_user_access(user: dict):
     elif user['age'] < 18:
         return 'Access denied: Underage'
     elif user['age'] >= 18 and user['is_active'] is True:
-        curr_role = user['roles']
+        curr_role = user['roles'][0]
         if curr_role == 'admin':
             return 'Access granted: Admin'
         elif curr_role == 'editor':
             return 'Access granted: Editor'
-    else:
-        return f'Access granted: {user['username']}'
+        else:
+            return f'Access granted: {user['username']}'
 
 
 # happy path ADMIN
@@ -20,9 +20,19 @@ user_dict = {'username': 'Max88verstappen', 'age': 18, 'is_active': True, 'roles
 inactive_user = {'username': 'joro_pochivka', 'age': 18, 'is_active': False, 'roles': ['admin']}
 
 # happy path EDITOR
-editor_dict = {'username': 'doom_guy', 'age': 18, 'is_active': True, 'roles': ['admin']}
+editor_dict = {'username': 'doom_guy', 'age': 18, 'is_active': True, 'roles': ['editor']}
 
 # underage acc
-underage_acc = {'username': 'doom_guy', 'age': 17, 'is_active': True, 'roles': ['admin']}
+underage_acc = {'username': 'little_kid', 'age': 17, 'is_active': True, 'roles': ['admin']}
+
+# just a user
+simple_man = {'username': 'simple_man', 'age': 18, 'is_active': True, 'roles': ['user']}
+
+
+print(check_user_access(user_dict))
+print(check_user_access(inactive_user))
+print(check_user_access(editor_dict))
+print(check_user_access(underage_acc))
+print(check_user_access(simple_man))
 
 
